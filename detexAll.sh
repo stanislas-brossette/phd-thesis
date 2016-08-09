018a17af303d12e6947187f3cc7e7485f780a5ae
stanislas-brossette <stanislas.brossette@gmail.com>
2016-08-09 10:01:51 +0900

    Typos

---
+++ detexAll.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+detex Appendix1-Optim/appendix1.tex > detexed/app1.txt
+detex Appendix2-Manifolds/appendix2.tex > detexed/app2.txt
+detex Chapter0-Introduction/chapter0.tex > detexed/ch0.txt
+detex Chapter1-State-of-the-art/chapter1.tex > detexed/ch1.txt
+detex Chapter2-PG/chapter2.tex > detexed/ch2.txt
+detex Chapter3-Extensions/chapter3.tex > detexed/ch3.txt
+detex Chapter4-Manifolds/chapter4.tex > detexed/ch4.txt
+detex Chapter5-NewPG/chapter5.tex > detexed/ch5.txt
+detex Chapter6-Evaluation/chapter6.tex > detexed/ch6.txt
+detex Chapter7-Conclusion/chapter7.tex > detexed/ch7.txt
